712be4ddea61b5c93eba284b6652f1987792ba09
Oliver Samuels <osamuels@bu.edu>
2024-01-31 12:47:38 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Alert,StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Video Game Rater</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        color = "blue"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
     </View>
   );
 }
